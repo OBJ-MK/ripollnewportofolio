b204e994282ad80735ecd45075be6933a79064a7
Modibo Kane <kaneferme1@gmail.com>
2026-08-02 21:29:29 +0000

    fixe de klk ptt beug de responsive

--- script/cms/main.js
+++ script/cms/main.js
@@ -16,17 +16,20 @@ import { loadTemoignages } from './components/temoignages.js';
 import { loadArticles, loadArticleDetailSPA } from './components/blog.js';
 import { loadSocialPosts } from './components/social.js';
 import { initWhatsappModal } from './components/contact-modal.js';
+import { initPosteModalListeners } from './modal/poste-modal.js';
 
 // Expose functions expected by inline HTML handlers
 window.navigateTo = navigateTo;
 window.navigateToSection = navigateToSection;
 window.closeProjetModal = closeProjetModal;
 window.initWhatsappModal = initWhatsappModal;
+window.initPosteModalListeners = initPosteModalListeners;
 // Initialize once DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
   // Ensure modal close/overlay handlers are wired
   initModalListeners();
   initWhatsappModal();
+  initPosteModalListeners();
 
   const hash = window.location.hash;
 
