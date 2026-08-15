7ff669c666bc7e510e6562384384dcc5e26ef875
Modibo Kane <kaneferme1@gmail.com>
2026-08-15 00:13:23 +0000

    securiter config 2

--- netlify/functions/sitemap.js
+++ netlify/functions/sitemap.js
@@ -72,6 +72,7 @@ ${body}
     headers: {
       'Content-Type': 'application/xml; charset=utf-8',
       'Cache-Control': 'public, max-age=3600', // 1h de cache, se régénère seul ensuite
+      'X-Content-Type-Options': 'nosniff',
     },
     body: xml,
   };
